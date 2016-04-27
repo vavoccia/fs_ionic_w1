d84743a46e1463f4b006dd27a256a9db0bf063dc
Vince Voccia <vvoccia@outlook.com>
2016-04-26 20:41:58 -0400

    Final Commit

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.services'])
+angular.module('conFusion', ['ionic', 'conFusion.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -36,8 +36,7 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
     url: '/home',
     views: {
       'mainContent': {
-        templateUrl: 'templates/home.html',
-        controller: 'IndexController'
+        templateUrl: 'templates/home.html'
       }
     }
   })
